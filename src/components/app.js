4c668801b30d713f0968e0dd76c309b441cd53ca
moriah-rahamim <moriahfaye@gmail.com>
2018-02-02 16:32:37 -0800

    render app and pass video data to videoList component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
 angular.module('video-player')
+  .component('app', {
+    controller: function($scope, $window) {
+      this.videos = $window.exampleVideoData;
+    },
 
-.component('app', {
-  // TODO
-});
+    templateUrl: 'src/templates/app.html'
+  });
