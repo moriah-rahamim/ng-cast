54bc40b63b494d3a7d05612998844a5aa9f7eb7f
moriah-rahamim <moriahfaye@gmail.com>
2018-02-02 19:30:44 -0800

    re-render video player on click

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 angular.module('video-player')
   .component('app', {
+
     controller: function($scope, $window) {
       this.videos = $window.exampleVideoData;
       this.onClick = (index) => {
         this.video = this.videos[index];
-        console.log(this.video, index);
       };
       this.video = this.videos[0];
     },
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,18 +1,8 @@
 angular.module('video-player')
 
   .component('videoPlayer', {
-    // TODO
     bindings: {
       video: '<'
-    },
-
-    controller: function() {
-      this.$onInit = () => {
-        this.videoId = this.video.id.videoId;
-        this.url = 'https://www.youtube.com/embed/' + this.videoId;
-        this.title = this.video.snippet.title;
-        this.description = this.video.snippet.description;
-      };
     },
 
     templateUrl: 'src/templates/videoPlayer.html'
