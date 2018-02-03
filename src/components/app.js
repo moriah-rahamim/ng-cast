c1f90511729e31cca897256929c8892b00d85833
moriah-rahamim <moriahfaye@gmail.com>
2018-02-03 11:25:12 -0800

    initialize app with live data

--- src/components/app.js
+++ src/components/app.js
@@ -2,16 +2,24 @@ angular.module('video-player')
   .component('app', {
 
     controller: function($scope, $window, youTube) {
-      this.videos = $window.exampleVideoData;
+
       this.selectVideo = (index) => {
         this.currentVideo = this.videos[index];
       };
+
       this.updateVideos = (data) => {
         this.videos = data.items;
         this.selectVideo(0);
       };
       this.updateVideos = this.updateVideos.bind(this);
-      this.currentVideo = this.videos[0];
+
+      this.$onInit = () => {
+        youTube.searchYouTube({
+          maxResults: 5,
+          key: $window.YOUTUBE_API_KEY,
+          query: 'kittens'
+        }, this.updateVideos);
+      };
     },
 
     templateUrl: 'src/templates/app.html'
